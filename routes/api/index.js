948cb46788b49ad47d0bec3f1fa122ce52e544be
Austin Miller <austin.miller1424@gmail.com>
2022-03-15 18:10:56 -0500

    pizza routes added

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+//imports all of the APU routes to prefix their enpoint names and packags them up 
+
+const router = require('express').Router();
+const pizzaRoutes = require('./pizza-routes');
+
+// add prefix of `/pizzas` to routes created in `pizza-routes.js`
+router.use('/pizzas', pizzaRoutes);
+
+module.exports = router;
+
